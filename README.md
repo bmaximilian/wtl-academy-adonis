0d90205045966b92689279499915371d0a8429d4
bmaximilian <maximilianbmail@gmail.com>
2018-10-25 10:27:52 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Adonis JS Blog
+AdonisJS Blog for Academy Day
+
+# Requirements
+* docker
+
+# Installation
+
+1. Copy `127.0.0.1 blog-api.test www.blog-api.test` to your `/etc/hosts`.
+3. Run `docker-compose -p blog up --build`
+4. Open **blog-api.test** in your browser
---
+++ api/.eslintrc.js
@@ -0,0 +1,62 @@
+process.chdir(__dirname);
+process.env.NODE_ENV = process.env.NODE_ENV ||'development';
+
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    globals: {
+        use: true,
+    },
+    extends: ['airbnb-base'],
+    rules: {
+        'import/extensions': ['error', 'always', {
+            'js': 'never',
+        }],
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
+        // allow console during development
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
+        'no-trailing-spaces': 'error',
+        'no-param-reassign': [2, { 'props': false }],
+        'class-methods-use-this': [0],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'always',
+        ],
+        indent: ['error', 4, {
+            MemberExpression: 0,
+            SwitchCase: 1,
+        }],
+        'no-multiple-empty-lines': ['error', { 'max': 1, 'maxBOF': 1 }],
+        'valid-jsdoc': 'error',
+        'arrow-body-style': 'off',
+        'max-len': ['error', { 'code': 120 }],
+        'require-jsdoc': ['error', {
+            require: {
+                FunctionDeclaration: true,
+                MethodDefinition: true,
+                ClassDeclaration: true,
+                ArrowFunctionExpression: true,
+                FunctionExpression: true,
+            },
+        }],
+        'prefer-destructuring': ['error', {
+            VariableDeclarator: {
+                array: true,
+                object: true
+            },
+            AssignmentExpression: {
+                array: false,
+                object: false,
+            },
+        }, {
+            'enforceForRenamedProperties': false
+        }]
+    },
+};
